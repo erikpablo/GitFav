eeac5f24a808c09073a758e84b123f8a0082e51a
Erik Pablo <nunespablo470@gmail.com>
2025-01-13 16:37:09 -0300

    feat

--- js/Favorites.js
+++ js/Favorites.js
@@ -8,4 +8,24 @@ export class Favorites {
 
 
 
-exp
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+
+        this.update()
+    }
+
+
+    update() {
+        this.removeAllTr()
+    }
+
+    removeAllTr() {
+        const tbody = this.root.querySelector('table tbody')
+
+        tbody.querySelectorAll('tr')
+        .forEach((tr) => {
+            tr.remove()
+        })
+    }
+}
