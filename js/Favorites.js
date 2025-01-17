6a1bbdf034809c5aba8d87862621e335a2484e41
Erik Pablo <nunespablo470@gmail.com>
2025-01-17 08:59:17 -0300

    feat

--- js/Favorites.js
+++ js/Favorites.js
@@ -20,6 +20,13 @@ export class Favorites {
 
     async add(username) {
         try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Usuário ja cadastrado')
+            }
+
             const user = await GithubUser.search(username)
             if(user.login === undefined) {
                 throw new Error('Usuário não encontrado!')
